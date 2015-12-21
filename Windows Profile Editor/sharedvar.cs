fc6ea8c16129e56803abcd3ee6cbdcb2531da427
Martino <martinojones_2009@hotmail.com>
2015-12-21 11:44:07 -0500

    Set icon and version
    Set the icon for the program as well as the version number.

--- Windows Profile Editor/sharedvar.cs
+++ Windows Profile Editor/sharedvar.cs
@@ -9,6 +9,9 @@ namespace Windows_Profile_Editor
 {
     class sharedvar
     {
+        //This is here so that I can easily find and edit variables that I share between the Form and inputtext code.
+        //I know I can just put this in the form code, but then I have to go searching for these variables, this is easier for me.
+
         public static string details = "EMPTY";
         public static string sid = "EMPTY";
         public static int closed = -1;
